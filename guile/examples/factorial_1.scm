4972e33e01bdb9e89874f0089845d54ccdff91ec
Pavel Tisnovsky <ptisnovs@redhat.com>
2019-07-15 08:11:50 +0200

    Factorial, recursive variant #1

---
+++ guile/examples/factorial_1.scm
@@ -0,0 +1,12 @@
+(define (print item)
+     (display item)
+     (newline))
+
+(define (factorial n)
+    (if (= n 0) ; podmínka pro ukončení rekurzivního zanořování
+        1       ; faktoriál nuly je definitoricky roven jedné
+        (* n (factorial (- n 1)))
+    )
+)
+
+(print (factorial 10))
