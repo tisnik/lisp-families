d8ed5abff29b9b02b87b29e701a75553590799e9
Pavel Tisnovsky <ptisnovs@redhat.com>
2019-07-22 08:00:55 +0200

    Do loop in Scheme, variant #2

---
+++ tinyscheme/examples/do_loop_2.scm
@@ -0,0 +1,4 @@
+(do ((i 1 (+ i 1)))  ; poèáteèní hodnota poèitadla a iteraèní výraz provedený na konci smyèky
+    ((= i 10))       ; podmínka vyhodnocovaná pro ukonèení smyèky
+        (display i)  ; tìlo smyèky
+        (newline))
