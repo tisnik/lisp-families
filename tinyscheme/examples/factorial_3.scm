45f11f0440be8b335b81fa10b5a83b7da6fe508d
Pavel Tisnovsky <ptisnovs@redhat.com>
2019-07-22 08:00:55 +0200

    Factorial, tail-call variant

---
+++ tinyscheme/examples/factorial_3.scm
@@ -0,0 +1,17 @@
+(define (factorial n)
+  (let fact-iter (        ; pomocná vnitřní funkce
+              (n n)       ; počitadlo iterací
+              (result 1)) ; průběžný výsledek
+    (if (= n 0)           ; po dosažení koncového stavu
+      result              ; se vrátí průběžný výsledek
+      (fact-iter (- n 1) (* n result)) ; koncové volání
+    )))
+
+
+(define n 0)
+
+(do ()
+  ((>= n 30))
+  (display (factorial n))
+  (newline)
+  (set! n (+ n 1)))
