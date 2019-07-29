c37947993a3ada3a25780e529e551dd9474f950d
Pavel Tisnovsky <ptisnovs@redhat.com>
2019-07-29 08:40:54 +0200

    Closure, example #1

---
+++ kawa/closure_1.scm
@@ -0,0 +1,17 @@
+(define (print item)
+     (display item)
+     (newline))
+
+(define (filter pred lst)
+  (cond ((null? lst) '())
+        ((pred (car lst))
+         (cons (car lst) (filter pred (cdr lst))))
+        (else (filter pred (cdr lst)))))
+
+(define (larger-than limit)
+    (lambda (value) (> value limit)))
+
+(print ((larger-than 5) 0))
+(print ((larger-than 5) 10))
+
+(print (filter (larger-than 5) '(1 2 3 4 5 6 7 8 9 10)))
