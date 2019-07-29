fe2c5fb027d02e56e11ab21bfbded92c7f27fb30
Pavel Tisnovsky <ptisnovs@redhat.com>
2019-07-29 08:40:54 +0200

    Pi computation, rational numbers variant

---
+++ kawa/pi_2.scm
@@ -0,0 +1,12 @@
+(define (compute-pi n)
+  (let ((pi 4))
+    (do ((i 3 (+ i 2)))
+      ((> i (+ n 2)))
+      (set! pi (* pi (/ (- i 1) i) (/ (+ i 1) i))))
+    pi))
+
+(do ((n 1 (* n 2)))
+  ((> n 10000))
+  (display n)
+  (display (compute-pi n))
+  (newline))
