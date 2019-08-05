2b1260a58877246deb663dff0432e42610a9d258
Pavel Tisnovsky <ptisnovs@redhat.com>
2019-08-05 08:09:43 +0200

    Ranges, 2nd example

---
+++ kawa/ranges2.scm
@@ -0,0 +1,16 @@
+(define abeceda "abcdefghijklmnopqrstuvwxyz")
+
+(display (abeceda [5 <=: 10]))
+(newline)
+
+(display (abeceda [5 by: 2 <=: 10]))
+(newline)
+
+(display (abeceda [20 >=: 5]))
+(newline)
+
+(display (abeceda [25 >=: 0]))
+(newline)
+
+(display (abeceda [<:]))
+(newline)
