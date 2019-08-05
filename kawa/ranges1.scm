5d3226ca7c3a1ac1f45fca28af2673063d1065a2
Pavel Tisnovsky <ptisnovs@redhat.com>
2019-08-05 08:09:43 +0200

    Ranges, 1st example

---
+++ kawa/ranges1.scm
@@ -0,0 +1,26 @@
+(display [1 <: 10])
+(newline)
+
+(display [1 <=: 10])
+(newline)
+
+(display [1 by: 2 <=: 10])
+(newline)
+
+(display [10 >: 0])
+(newline)
+
+(display [10 by: -2 >: 0])
+(newline)
+
+(display [10 >=: 0])
+(newline)
+
+(display [10 by: -2 >=: 0])
+(newline)
+
+(display [1 by: 1/2 <=: 10])
+(newline)
+
+(display [0 by: 1/10 <=: 1])
+(newline)
