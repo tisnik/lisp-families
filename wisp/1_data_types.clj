6ae28458493a3bbfd1be2ab828317b2254d15068
Pavel Tisnovsky <ptisnovs@redhat.com>
2019-08-13 15:31:09 +0200

    1st Wisp example - basic data types

---
+++ wisp/1_data_types.clj
@@ -0,0 +1,41 @@
+; Examples for Wisp: Homoiconic JS with clojure syntax, s-expressions & macros
+;
+;  (C) Copyright 2015  Pavel Tisnovsky
+;
+;  All rights reserved. This program and the accompanying materials
+;  are made available under the terms of the Eclipse Public License v1.0
+;  which accompanies this distribution, and is available at
+;  http://www.eclipse.org/legal/epl-v10.html
+;
+;  Contributors:
+;      Pavel Tisnovsky
+;
+
+; Test překladu konstant základních datových typů
+; do JavaScriptu
+
+
+; "not in list"
+nil
+
+; pravdivostní hodnoty
+true
+
+false
+
+; numerické hodnoty
+42
+
+1.2M
+
+2/3
+
+; znaky
+\a
+\@
+
+; řetězce
+"Hello world!"
+
+:keyword
+
