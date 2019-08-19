63765086b6ed516dfc582671610e9db72cc90534
Pavel Tisnovsky <ptisnovs@redhat.com>
2019-08-19 12:42:45 +0200

    Closure, example #3

---
+++ racket/closure_3.rkt
@@ -0,0 +1,15 @@
+#lang racket/base
+
+(define (print item)
+     (display item)
+     (newline))
+
+(define counter
+    (let ((i -1))
+         (lambda ()
+             (set! i (+ i 1))
+             i)))
+
+(print (counter))
+(print (counter))
+(print (counter))
