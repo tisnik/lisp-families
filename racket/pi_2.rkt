28621c9f72996fc3d6445d92e1c54d4ed10b9bef
Pavel Tisnovsky <ptisnovs@redhat.com>
2019-08-19 12:42:45 +0200

    Pi calculation, usage of rational numbers

---
+++ racket/pi_2.rkt
@@ -0,0 +1,15 @@
+#lang racket/base
+
+(define (compute-pi n)
+  (let ((pi 4))
+    (do ((i 3 (+ i 2)))
+      ((> i (+ n 2)))
+      (set! pi (* pi (/ (- i 1) i) (/ (+ i 1) i))))
+    pi))
+
+(do ((n 1 (* n 2)))
+  ((> n 100))
+  (display n)
+  (display " ")
+  (display (compute-pi n))
+  (newline))
