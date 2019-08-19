791ea8a0f7e17c682108986aff6fecf758420033
Pavel Tisnovsky <ptisnovs@redhat.com>
2019-08-19 12:42:45 +0200

    Function with variable number of arguments

---
+++ racket/func_vararg.rkt
@@ -0,0 +1,14 @@
+#lang racket/base
+
+(define (print item)
+     (display item)
+     (newline))
+
+; funkce vracející počet skutečně předaných parametrů
+(define (foo . parametry) (length parametry))
+
+(print (foo))
+(print (foo 1))
+(print (foo 1 2))
+(print (foo "bar" "baz"))
+(print (foo '(1 2 3 4)))
