f3c0882d07d6919148c04aef74b9e02f1e77b5eb
Pavel Tisnovsky <ptisnovs@redhat.com>
2019-08-22 17:34:20 +0200

    Clojure-py example #1: basic data types

---
+++ clojure-py/01_data_types.clj
@@ -0,0 +1,45 @@
+; Examples for Clojure-py a dialect of the Lisp programming language designed to interact with Python
+;
+;  (C) Copyright 2018  Pavel Tisnovsky
+;
+;  All rights reserved. This program and the accompanying materials
+;  are made available under the terms of the Eclipse Public License v1.0
+;  which accompanies this distribution, and is available at
+;  http://www.eclipse.org/legal/epl-v10.html
+;
+;  Contributors:
+;      Pavel Tisnovsky
+;
+
+; Základní datové typy
+
+
+; "not in list"
+nil
+
+
+; pravdivostní hodnoty
+true
+
+false
+
+
+; numerické hodnoty
+;   celá čísla
+42
+0x2a
+
+;   čísla s plovoucí řádovou čárkou
+3.14
+
+;   zlomky
+;;;2/3
+
+
+; řetězce
+"Hello world!"
+
+
+; klíčová slova (speciální třída symbolů)
+:keyword
+
