5a09bb799b5a814f97a3cc3d37cc66e0d120f3af
Pavel Tisnovsky <ptisnovs@redhat.com>
2019-09-02 14:21:02 +0200

    Hygienic swap

---
+++ racket/macros/swap4.rkt
@@ -0,0 +1,12 @@
+(define-syntax-rule (swap x y)
+    (let ([tmp x])
+         (set! x y)
+         (set! y tmp)))
+
+(let [(tmp 10)
+      (set! 20)]
+     (swap tmp set!)
+     (display tmp)
+     (newline)
+     (display set!)
+     (newline))
