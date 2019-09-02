47e06dfe5ea46b11e6fc316c7d14be0fb317c1b7
Pavel Tisnovsky <ptisnovs@redhat.com>
2019-09-02 14:34:26 +0200

    Printable macro expansion

---
+++ racket/macros/or1-printable.rkt
@@ -0,0 +1,12 @@
+(define-syntax or
+  (syntax-rules ()
+    [(or) #f]
+    [(or x) x]
+    [(or x y) (let ([z x])
+                   (if z z y))]))
+
+(display (syntax->datum (expand-once '(or))))
+(display (syntax->datum (expand-once '(or #f))))
+(display (syntax->datum (expand-once '(or #t))))
+(display (syntax->datum (expand-once '(or #f #f))))
+(display (syntax->datum (expand-once '(or #f #t))))
