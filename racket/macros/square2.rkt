7f5769b3e496e1e9eb7a073432aff99ae3eb21e2
Pavel Tisnovsky <ptisnovs@redhat.com>
2019-09-02 14:13:49 +0200

    Better macro to compute square of given value

---
+++ racket/macros/square2.rkt
@@ -0,0 +1,21 @@
+(define-syntax-rule (square x)
+    (let ([tmp x])
+         (* tmp tmp)))
+
+(display (square 10))
+(newline)
+
+(display (expand-once (square 10)))
+(newline)
+
+(display (expand-once '(square 10)))
+(newline)
+
+(display (expand-once '(square 10)))
+(newline)
+
+(display (expand '(square 10)))
+(newline)
+
+(display (expand-once '(square (+ 1 2))))
+(newline)
