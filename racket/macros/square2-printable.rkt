d5127178bc15d5e692de248e6b4f0d4d44b2079b
Pavel Tisnovsky <ptisnovs@redhat.com>
2019-09-02 14:34:37 +0200

    Printable macro expansion

---
+++ racket/macros/square2-printable.rkt
@@ -0,0 +1,21 @@
+(define-syntax-rule (square x)
+    (let ([tmp x])
+         (* tmp tmp)))
+
+(display (square 10))
+(newline)
+
+(display (syntax->datum (expand-once (square 10))))
+(newline)
+
+(display (syntax->datum (expand-once '(square 10))))
+(newline)
+
+(display (syntax->datum (expand-once '(square 10))))
+(newline)
+
+(display (syntax->datum (expand '(square 10))))
+(newline)
+
+(display (syntax->datum (expand-once '(square (+ 1 2)))))
+(newline)
