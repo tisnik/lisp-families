1c75accff10384f0dda324dac6bba0bf8b7e19a2
Pavel Tisnovsky <ptisnovs@redhat.com>
2019-10-07 21:17:21 +0200

    Basic data types

---
+++ joker/01_data_types.clj
@@ -0,0 +1,32 @@
+; Základní datové typy
+
+
+; "not in list"
+nil
+
+
+; pravdivostní hodnoty
+true
+
+false
+
+
+; numerické hodnoty
+;   celá čísla
+42
+0x2a
+
+;   čísla s plovoucí řádovou čárkou
+3.14
+
+;   zlomky
+;;;2/3
+
+
+; řetězce
+"Hello world!"
+
+
+; klíčová slova (speciální třída symbolů)
+:keyword
+
