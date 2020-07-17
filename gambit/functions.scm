775420960992dbb42c7c82e4653a98b2da330a8f
Pavel Tisnovsky <ptisnovs@redhat.com>
2020-07-17 10:42:46 +0200

    Functions and lambdas

---
+++ gambit/functions.scm
@@ -0,0 +1,36 @@
+(define (print item)
+     (display item)
+     (newline))
+
+; anonymous function
+(lambda (x y) (+ x y))
+
+; anonymous function is a value
+(print (lambda (x y) (+ x y)))
+
+; apply anonymous function
+(print
+  (
+    ; create anonymous function
+    (lambda (x y) (+ x y))
+    ; pass parameters to it
+    1 2))
+
+; apply anonymous function (one liner)
+(print ((lambda (x y) (+ x y)) 1 2))
+
+; one-liner function
+(define (add x y) (+ x y))
+
+; function written on more lines
+(define (mul x y)
+  (* x y))
+
+; function written on more lines using lambda
+(define div (lambda (x y)
+  (* x y)))
+
+; test functions
+(print (add 1 2))
+(print (mul 6 7))
+(print (div 10 3))
