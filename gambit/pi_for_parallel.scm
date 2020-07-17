b2601951b219aa59c52215aab940dd31729d2f47
Pavel Tisnovsky <ptisnovs@redhat.com>
2020-07-17 10:42:46 +0200

    Preparation for parallel Pi computation

---
+++ gambit/pi_for_parallel.scm
@@ -0,0 +1,19 @@
+(define (compute-pi n)
+  (let ((pi 4.0))
+    (do ((i 3 (+ i 2)))
+      ((> i (+ n 2)))
+      (set! pi (* pi (/ (- i 1) i) (/ (+ i 1) i))))
+    pi))
+ 
+(define (pi-for n)
+  (display n)
+  (display "\t")
+  (display (compute-pi n))
+  (newline))
+
+(define (power-of-two n maximum)
+  (if (< n maximum)
+     (cons n (power-of-two (* n 2) maximum))
+     (list n)))
+
+(map pi-for (power-of-two 1 5000000))
