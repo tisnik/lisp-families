4b475813b95b3be1bbd50f1ca62a8ad87c55c9b2
Pavel Tisnovsky <ptisnovs@redhat.com>
2020-12-14 10:11:03 +0100

    Reformatted lexical_scope_2.scm according to Scheme standard

--- gambit/lexical_scope_2.scm
+++ gambit/lexical_scope_2.scm
@@ -1,12 +1,12 @@
 (define (print item)
-     (display item)
-     (newline))
+  (display item)
+  (newline))
 
 (define x 1)
 (define y 2)
 
 (define (add x y)
-    (+ x y))
+  (+ x y))
 
 (print (add x y))
 (set! x 10)
