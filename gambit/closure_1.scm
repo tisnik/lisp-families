d6cc4ed2e41cfc715c7de5a316e215bdf13bc7c8
Pavel Tisnovsky <ptisnovs@redhat.com>
2020-12-14 10:11:03 +0100

    Reformatted closure_1.scm according to Scheme standard

--- gambit/closure_1.scm
+++ gambit/closure_1.scm
@@ -1,12 +1,12 @@
 (define (print item)
-     (display item)
-     (newline))
+  (display item)
+  (newline))
 
 (define counter
-    (let ((i -1))
-         (lambda ()
-             (set! i (+ i 1))
-             i)))
+  (let ((i -1))
+    (lambda ()
+      (set! i (+ i 1))
+      i)))
 
 (print (counter))
 (print (counter))
