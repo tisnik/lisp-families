22ada8e04c915f7dc6dfdacad0f0cab0acf64f56
Pavel Tisnovsky <ptisnovs@redhat.com>
2020-12-14 10:11:11 +0100

    Optional and rest parameters to functions

---
+++ gambit/functions_optional_rest.scm
@@ -0,0 +1,23 @@
+(define (print item)
+  (display item)
+  (newline))
+
+(define (f1 #!optional (a 4) #!key (b 5) c)
+  (print a)
+  (print b)
+  (print c)
+  (newline))
+
+(define (f2 #!key (i 5) #!rest j)
+  (print i)
+  (print j)
+  (newline))
+
+(f1)
+(f1 1)
+(f1 2 b: 3)
+(f1 4 b: 5 c: 6)
+(f1 7 c: 8)
+(f2 9)
+(f2 10 i: 11)
+(f2 i: 12 13)
