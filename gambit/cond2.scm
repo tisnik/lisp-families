7bd92e5fa5d43aecd7e98decf8d942d81790585e
Pavel Tisnovsky <ptisnovs@redhat.com>
2020-12-14 10:11:11 +0100

    Cond special form

---
+++ gambit/cond2.scm
@@ -0,0 +1,16 @@
+(define (print item)
+  (display item)
+  (newline))
+
+(define (sgn n)
+  (cond
+    ((not (number? n)) 'not-a-number)
+    ((< n 0)      'negative)
+    ((> n 0)      'positive)
+    ((zero? n)    'zero)))
+
+(print (sgn -10))
+(print (sgn 0))
+(print (sgn 10))
+
+(print (sgn '()))
