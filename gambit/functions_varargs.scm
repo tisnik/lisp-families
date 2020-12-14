acc342ccd9f660619dec4e9370955f2357b5356b
Pavel Tisnovsky <ptisnovs@redhat.com>
2020-12-14 10:11:11 +0100

    Vararg functions

---
+++ gambit/functions_varargs.scm
@@ -0,0 +1,36 @@
+(define (print item)
+  (display item)
+  (newline))
+
+(define (f1)
+  (print "no parameters"))
+
+(define (f2 a)
+  (print "one parameter")
+  (print a))
+
+(define (f3 a b)
+  (print "two parameters")
+  (print a)
+  (print b))
+
+(define (f4 a . b)
+  (print "at least one parameter")
+  (print a)
+  (print b))
+
+(define (f5 a b . c)
+  (print "at least two parameters")
+  (print a)
+  (print b)
+  (print c))
+
+(f1)
+(f2 10)
+(f3 1 2)
+(f4 1)
+(f4 1 2)
+(f4 1 2 3)
+(f5 1 2)
+(f5 1 2 3)
+(f5 1 2 3 4)
