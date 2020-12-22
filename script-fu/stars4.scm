f16d63766b9e59f6a68740d23497ca9b72e1e7f5
Pavel Tisnovsky <ptisnovs@redhat.com>
2020-12-22 08:49:40 +0100

    Added license header (issue #43)

--- script-fu/stars4.scm
+++ script-fu/stars4.scm
@@ -1,3 +1,17 @@
+; Skriptování v GIMPu
+
+;
+;  (C) Copyright 2020  Pavel Tisnovsky
+;
+;  All rights reserved. This program and the accompanying materials
+;  are made available under the terms of the Eclipse Public License v1.0
+;  which accompanies this distribution, and is available at
+;  http://www.eclipse.org/legal/epl-v10.html
+;
+;  Contributors:
+;      Pavel Tisnovsky
+;
+
 ; Funkce, která vytvoří nový obrázek o zadané velikosti.
 ; V obrázku bude jedna hladina nazvaná "Stars" a
 ; v této hladině bude vykreslena hvězdná obloha.
