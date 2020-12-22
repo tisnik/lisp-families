335be4720ad5843fa72901b8bf74fbd52af9e25b
Pavel Tisnovsky <ptisnovs@redhat.com>
2020-12-22 08:48:41 +0100

    Added license header (issue #49)

--- script-fu/selection1.scm
+++ script-fu/selection1.scm
@@ -1,3 +1,17 @@
+; Skriptování v GIMPu
+
+;
+;  (C) Copyright 2020  Pavel Tisnovsky
+;
+;  All rights reserved. This program and the accompanying materials
+;  are made available under the terms of the Eclipse Public License v1.0
+;  which accompanies this distribution, and is available at
+;  http://www.eclipse.org/legal/epl-v10.html
+;
+;  Contributors:
+;      Pavel Tisnovsky
+;
+
 ; Tato funkce je zavolána z dialogu vyvolaného uživatelem
 ; z grafického uživatelského rozhraní GIMPu.
 (define (script-draw-circle image-width image-height radius selected-color background-color)
