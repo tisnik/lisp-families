a13e7d6148d29ffd7129a559b269e4c925b041b1
Pavel Tisnovsky <ptisnovs@redhat.com>
2020-12-22 08:48:53 +0100

    Added license header (issue #56)

--- script-fu/selection.scm
+++ script-fu/selection.scm
@@ -1,6 +1,18 @@
 ; Skriptování v GIMPu (3)
 ; Demonstrační příklad číslo 2
 
+;
+;  (C) Copyright 2020  Pavel Tisnovsky
+;
+;  All rights reserved. This program and the accompanying materials
+;  are made available under the terms of the Eclipse Public License v1.0
+;  which accompanies this distribution, and is available at
+;  http://www.eclipse.org/legal/epl-v10.html
+;
+;  Contributors:
+;      Pavel Tisnovsky
+;
+
 ; Tato funkce je zavolána z dialogu vyvolaného uživatelem
 ; z grafického uživatelského rozhraní GIMPu.
 (define (script-draw-selection
