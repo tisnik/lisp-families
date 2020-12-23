1a59e75bae6fa6d78c4484197a96ad1ac84679c2
Pavel Tisnovsky <ptisnovs@redhat.com>
2020-12-23 08:52:00 +0100

    Added license header (issue #70)

--- tinyscheme/examples/nested_loops_2.scm
+++ tinyscheme/examples/nested_loops_2.scm
@@ -1,3 +1,15 @@
+;
+;  (C) Copyright 2020  Pavel Tisnovsky
+;
+;  All rights reserved. This program and the accompanying materials
+;  are made available under the terms of the Eclipse Public License v1.0
+;  which accompanies this distribution, and is available at
+;  http://www.eclipse.org/legal/epl-v10.html
+;
+;  Contributors:
+;      Pavel Tisnovsky
+;
+
 (do ((y 1 (+ y 1)))    ; počáteční hodnota počitadla a iterační příkaz
   ((> y 10))           ; podmínka pro ukončení smyčky
   (do ((x 1 (+ x 1)))  ; vnitřní smyčka
