c80f4643f90bd342e0184c741da41cb032df31c1
Pavel Tisnovsky <ptisnovs@redhat.com>
2020-12-24 09:55:20 +0100

    Cond special form, second example

---
+++ guile/examples/cond2.scm
@@ -0,0 +1,30 @@
+; Examples for Guile 2.x
+
+;
+;  (C) Copyright 2020  Pavel Tisnovsky
+;
+;  All rights reserved. This program and the accompanying materials
+;  are made available under the terms of the Eclipse Public License v1.0
+;  which accompanies this distribution, and is available at
+;  http://www.eclipse.org/legal/epl-v10.html
+;
+;  Contributors:
+;      Pavel Tisnovsky
+;
+
+(define (print item)
+  (display item)
+  (newline))
+
+(define (sgn n)
+  (cond
+    ((not (number? n)) 'not-a-number)
+    ((< n 0)      'negative)
+    ((> n 0)      'positive)
+    ((zero? n)    'zero)))
+
+(print (sgn -10))
+(print (sgn 0))
+(print (sgn 10))
+
+(print (sgn '()))
