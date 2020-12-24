74cfd5b1c059cb258ea3c18e0b9a91d71828c920
Pavel Tisnovsky <ptisnovs@redhat.com>
2020-12-24 10:03:59 +0100

    Added license header (issue #44)

--- guile/examples/closure_2.scm
+++ guile/examples/closure_2.scm
@@ -1,14 +1,28 @@
+; Examples for Guile 2.x
+
+;
+;  (C) Copyright 2020  Pavel Tisnovsky
+;
+;  All rights reserved. This program and the accompanying materials
+;  are made available under the terms of the Eclipse Public License v1.0
+;  which accompanies this distribution, and is available at
+;  http://www.eclipse.org/legal/epl-v10.html
+;
+;  Contributors:
+;      Pavel Tisnovsky
+;
+
 (define (print item)
-     (display item)
-     (newline))
+  (display item)
+  (newline))
 
 (define (larger-than limit)
-    (lambda (value) (> value limit)))
+  (lambda (value) (> value limit)))
 
 (print (filter (larger-than 5) '(1 2 3 4 5 6 7 8 9 10)))
 
 (define (larger-than-five)
-     (larger-than 5))
+  (larger-than 5))
 
 (print (filter (larger-than-five) '(1 2 3 4 5 6 7 8 9 10)))
 
