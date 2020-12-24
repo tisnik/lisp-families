bed821ed5e09099777e7a046b55e83455fcd6559
Pavel Tisnovsky <ptisnovs@redhat.com>
2020-12-24 10:04:39 +0100

    Added license header (issue #85)

--- guile/examples/functions.scm
+++ guile/examples/functions.scm
@@ -1,6 +1,20 @@
+; Examples for Guile 2.x
+
+;
+;  (C) Copyright 2020  Pavel Tisnovsky
+;
+;  All rights reserved. This program and the accompanying materials
+;  are made available under the terms of the Eclipse Public License v1.0
+;  which accompanies this distribution, and is available at
+;  http://www.eclipse.org/legal/epl-v10.html
+;
+;  Contributors:
+;      Pavel Tisnovsky
+;
+
 (define (print item)
-     (display item)
-     (newline))
+  (display item)
+  (newline))
 
 ; anonymous function
 (lambda (x y) (+ x y))
