c2999f8c54fe43d9c1589f6afd7bae986e2b44b2
Pavel Tisnovsky <ptisnovs@redhat.com>
2020-12-25 10:21:30 +0100

    Close #77

--- guile/c-interface/test7/script7.scm
+++ guile/c-interface/test7/script7.scm
@@ -1,6 +1,20 @@
+; Examples for Guile 2.x
+
+;
+;  (C) Copyright 2020  Pavel Tisnovsky
+;
+;  All rights reserved. This program and the accompanying materials
+;  are made available under the terms of the Eclipse Public License v1.0
+;  which accompanies this distribution, and is available at
+;  http://www.eclipse.org/legal/epl-v10.html
+;
+;  Contributors:
+;      Pavel Tisnovsky
+;
+
 (define (print-list lst)
   (begin
-      (display (length lst))
-      (newline)
-      (display lst)
-      (newline)))
+    (display (length lst))
+    (newline)
+    (display lst)
+    (newline)))
