3a698cec8bf76dfd70a8d045551f2e5d1f184552
Pavel Tisnovsky <ptisnovs@redhat.com>
2020-12-25 10:14:43 +0100

    Fix issue #80

--- guile/c-interface/test1/script1.scm
+++ guile/c-interface/test1/script1.scm
@@ -1,2 +1,16 @@
+; Examples for Guile 2.x
+
+;
+;  (C) Copyright 2020  Pavel Tisnovsky
+;
+;  All rights reserved. This program and the accompanying materials
+;  are made available under the terms of the Eclipse Public License v1.0
+;  which accompanies this distribution, and is available at
+;  http://www.eclipse.org/legal/epl-v10.html
+;
+;  Contributors:
+;      Pavel Tisnovsky
+;
+
 (define (say-hello)
   (display "Hello world!"))
