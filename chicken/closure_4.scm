59763d9c44a2bb146457ccf22f8ebd512404be2d
Pavel Tisnovsky <ptisnovs@redhat.com>
2021-02-09 08:41:30 +0100

    Added comments into chicken/closure_4.scm

--- chicken/closure_4.scm
+++ chicken/closure_4.scm
@@ -1,7 +1,7 @@
 ; Examples prepared for Chicken Scheme
 
 ;
-;  (C) Copyright 2020  Pavel Tisnovsky
+;  (C) Copyright 2020, 2021  Pavel Tisnovsky
 ;
 ;  All rights reserved. This program and the accompanying materials
 ;  are made available under the terms of the Eclipse Public License v1.0
@@ -14,19 +14,24 @@
 
 (use srfi-1)
 
+; helper function to print a value with newline appended
 (define (print item)
      (display item)
      (newline))
 
+; closure construction
 (define (get-counter)
     (let ((i -1))
+         ; function with value closed over
          (lambda ()
              (set! i (+ i 1))
              i)))
 
+; construct two closures
 (define counter1 (get-counter))
 (define counter2 (get-counter))
 
+; usage of closures
 (print (counter1))
 (print (counter1))
 (print (counter1))
@@ -38,4 +43,3 @@
 (print (counter1))
 (print (counter1))
 (print (counter1))
-
