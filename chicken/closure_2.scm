1bf2f990bebac1c7f26e365064e13fec09f7045b
Pavel Tisnovsky <ptisnovs@redhat.com>
2021-02-09 08:41:30 +0100

    Added comments into chicken/closure_2.scm

--- chicken/closure_2.scm
+++ chicken/closure_2.scm
@@ -1,7 +1,7 @@
 ; Examples prepared for Chicken Scheme
 
 ;
-;  (C) Copyright 2020  Pavel Tisnovsky
+;  (C) Copyright 2020, 2021  Pavel Tisnovsky
 ;
 ;  All rights reserved. This program and the accompanying materials
 ;  are made available under the terms of the Eclipse Public License v1.0
@@ -14,23 +14,29 @@
 
 (use srfi-1)
 
+; helper function to print a value with newline appended
 (define (print item)
      (display item)
      (newline))
 
+; closure construction
 (define (larger-than limit)
+    ; function with value closed over
     (lambda (value) (> value limit)))
 
+; construct and use closure
 (print (filter (larger-than 5) '(1 2 3 4 5 6 7 8 9 10)))
 
 (define (larger-than-five)
      (larger-than 5))
 
+; another usage of closure
 (print (filter (larger-than-five) '(1 2 3 4 5 6 7 8 9 10)))
 
+; another usage of closure
 (print ((larger-than 5) 0))
 (print ((larger-than 5) 10))
 
-
+; yet another usage of closure
 (print ((larger-than-five) 0))
 (print ((larger-than-five) 10))
