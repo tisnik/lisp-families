5f92da646aecd4b8cb239abe4370f37daae4726c
Pavel Tisnovsky <ptisnovs@redhat.com>
2021-02-10 09:08:47 +0100

    Added comments into femtolisp/closure_2.scm

--- femtolisp/closure_2.scm
+++ femtolisp/closure_2.scm
@@ -1,7 +1,7 @@
 ; Examples prepared for Femtolisp
 
 ;
-;  (C) Copyright 2020  Pavel Tisnovsky
+;  (C) Copyright 2020, 2021  Pavel Tisnovsky
 ;
 ;  All rights reserved. This program and the accompanying materials
 ;  are made available under the terms of the Eclipse Public License v1.0
@@ -12,19 +12,24 @@
 ;      Pavel Tisnovsky
 ;
 
+; helper function to print a value with newline appended
 (define (print item)
      (princ item)
      (newline))
 
+; closure construction
 (define (get-counter)
     (let ((i -1))
+         ; function with value closed over
          (lambda ()
              (set! i (+ i 1))
              i)))
 
+; construct two closures
 (define counter1 (get-counter))
 (define counter2 (get-counter))
 
+; usage of closures
 (print "Counter 1")
 (print (counter1))
 (print (counter1))
