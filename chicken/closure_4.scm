02513c92e6b18b1709768c7653d873e00a65faf0
Pavel Tisnovsky <ptisnovs@redhat.com>
2021-02-11 08:29:19 +0100

    Fixed indentation in closure_4.scm

--- chicken/closure_4.scm
+++ chicken/closure_4.scm
@@ -16,16 +16,16 @@
 
 ; helper function to print a value with newline appended
 (define (print item)
-     (display item)
-     (newline))
+  (display item)
+  (newline))
 
 ; closure construction
 (define (get-counter)
-    (let ((i -1))
-         ; function with value closed over
-         (lambda ()
-             (set! i (+ i 1))
-             i)))
+  (let ((i -1))
+    ; function with value closed over
+    (lambda ()
+      (set! i (+ i 1))
+      i)))
 
 ; construct two closures
 (define counter1 (get-counter))
