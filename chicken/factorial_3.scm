4067417b99f3b90fdd101244aaacbed3f682f4db
Pavel Tisnovsky <ptisnovs@redhat.com>
2021-02-12 08:57:11 +0100

    Comments + indentation in factorial_3.scm

--- chicken/factorial_3.scm
+++ chicken/factorial_3.scm
@@ -1,7 +1,7 @@
 ; Examples prepared for Chicken Scheme
 
 ;
-;  (C) Copyright 2020  Pavel Tisnovsky
+;  (C) Copyright 2020, 2021  Pavel Tisnovsky
 ;
 ;  All rights reserved. This program and the accompanying materials
 ;  are made available under the terms of the Eclipse Public License v1.0
@@ -12,17 +12,18 @@
 ;      Pavel Tisnovsky
 ;
 
+; helper function to print a value with newline appended
 (define (print item)
-     (display item)
-     (newline))
+  (display item)
+  (newline))
 
 (define (factorial n)
-    (let fact-iter (          ; pomocná vnitřní funkce
-                  (n n)       ; počitadlo iterací
-                  (result 1)) ; průběžný výsledek
-        (if (= n 0)           ; po dosažení koncového stavu
-            result            ; se vrátí průběžný výsledek
-            (fact-iter (- n 1) (* n result)) ; koncové volání
-        )))
+  (let fact-iter (          ; pomocná vnitřní funkce
+                (n n)       ; počitadlo iterací
+                (result 1)) ; průběžný výsledek
+      (if (= n 0)           ; po dosažení koncového stavu
+          result            ; se vrátí průběžný výsledek
+          (fact-iter (- n 1) (* n result)) ; koncové volání
+      )))
 
 (print (factorial 10))
