c9c1a389a8f4105d7c5dcaeff69624d481694360
Pavel Tisnovsky <ptisnovs@redhat.com>
2021-02-15 09:00:29 +0100

    Updated comments in cons.scm

--- guile/examples/cons.scm
+++ guile/examples/cons.scm
@@ -1,7 +1,7 @@
 ; Examples for Guile 2.x
 
 ;
-;  (C) Copyright 2020  Pavel Tisnovsky
+;  (C) Copyright 2020, 2021  Pavel Tisnovsky
 ;
 ;  All rights reserved. This program and the accompanying materials
 ;  are made available under the terms of the Eclipse Public License v1.0
@@ -12,19 +12,24 @@
 ;      Pavel Tisnovsky
 ;
 
+; helper function to print a value with newline appended
 (define (print item)
   (display item)
   (newline))
 
+; cons usage
 (print (cons 1 2))
 
+; another cons usage
 (print (cons 1 (cons 2 3)))
 
+; dot-pairs
 (print '((1 . 2) . (3 . 4)))
 
 ; this is proper list
 (print (cons 1 (cons 2 (cons 3 '()))))
 
+; poor man's nil (not fully compatible with LISP)
 (define nil '())
 
 ; this is proper list
