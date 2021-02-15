709f3fa3ff134a0f95ea4c6559f4f8cd9532548b
Pavel Tisnovsky <ptisnovs@redhat.com>
2021-02-15 09:00:29 +0100

    Updated comments in factorial1.scm

--- guile/examples/factorial_1.scm
+++ guile/examples/factorial_1.scm
@@ -1,7 +1,7 @@
 ; Examples for Guile 2.x
 
 ;
-;  (C) Copyright 2020  Pavel Tisnovsky
+;  (C) Copyright 2020, 2021  Pavel Tisnovsky
 ;
 ;  All rights reserved. This program and the accompanying materials
 ;  are made available under the terms of the Eclipse Public License v1.0
@@ -12,6 +12,7 @@
 ;      Pavel Tisnovsky
 ;
 
+; helper function to print a value with newline appended
 (define (print item)
   (display item)
   (newline))
@@ -20,7 +21,7 @@
   (if (= n 0) ; podmínka pro ukončení rekurzivního zanořování
       1       ; faktoriál nuly je definitoricky roven jedné
       (* n (factorial (- n 1)))
+      )
   )
-)
 
 (print (factorial 10))
