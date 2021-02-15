1c22ef6a098837fabaa1e44262372cc6e228302f
Pavel Tisnovsky <ptisnovs@redhat.com>
2021-02-15 09:00:29 +0100

    Updated comments in closure_1.scm

--- guile/examples/closure_1.scm
+++ guile/examples/closure_1.scm
@@ -1,7 +1,7 @@
 ; Examples for Guile 2.x
 
 ;
-;  (C) Copyright 2020  Pavel Tisnovsky
+;  (C) Copyright 2020, 2021  Pavel Tisnovsky
 ;
 ;  All rights reserved. This program and the accompanying materials
 ;  are made available under the terms of the Eclipse Public License v1.0
@@ -12,14 +12,19 @@
 ;      Pavel Tisnovsky
 ;
 
+; helper function to print a value with newline appended
 (define (print item)
   (display item)
   (newline))
 
+; closure construction
 (define (larger-than limit)
+  ; function with value closed over
   (lambda (value) (> value limit)))
 
+; construct and use closure
 (print ((larger-than 5) 0))
 (print ((larger-than 5) 10))
 
+; another usage of closure
 (print (filter (larger-than 5) '(1 2 3 4 5 6 7 8 9 10)))
