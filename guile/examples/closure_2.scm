0a687c9fadafdf972eff353999e853f5751a7802
Pavel Tisnovsky <ptisnovs@redhat.com>
2021-02-15 09:00:29 +0100

    Updated comments in closure_2.scm

--- guile/examples/closure_2.scm
+++ guile/examples/closure_2.scm
@@ -12,23 +12,29 @@
 ;      Pavel Tisnovsky
 ;
 
+; helper function to print a value with newline appended
 (define (print item)
   (display item)
   (newline))
 
+; closure construction
 (define (larger-than limit)
+  ; function with value closed over
   (lambda (value) (> value limit)))
 
+; construct and use closure
 (print (filter (larger-than 5) '(1 2 3 4 5 6 7 8 9 10)))
 
 (define (larger-than-five)
   (larger-than 5))
 
+; another usage of closure
 (print (filter (larger-than-five) '(1 2 3 4 5 6 7 8 9 10)))
 
+; another usage of closure
 (print ((larger-than 5) 0))
 (print ((larger-than 5) 10))
 
-
+; yet another usage of closure
 (print ((larger-than-five) 0))
 (print ((larger-than-five) 10))
