fd0a1e29adc62e0b23c60d8858bb70f388156e1f
Pavel Tisnovsky <ptisnovs@redhat.com>
2021-02-15 09:00:29 +0100

    Updated comments in dot_pairs.scm

--- guile/examples/dot_pairs.scm
+++ guile/examples/dot_pairs.scm
@@ -1,7 +1,7 @@
 ; Examples for Guile 2.x
 
 ;
-;  (C) Copyright 2020  Pavel Tisnovsky
+;  (C) Copyright 2020, 2021  Pavel Tisnovsky
 ;
 ;  All rights reserved. This program and the accompanying materials
 ;  are made available under the terms of the Eclipse Public License v1.0
@@ -12,14 +12,18 @@
 ;      Pavel Tisnovsky
 ;
 
+; helper function to print a value with newline appended
 (define (print item)
   (display item)
   (newline))
 
+; usage of so-called dot pairs
 (print '(1 . 2))
 
+; another combination of dot-pairs
 (print '(1 . ((2 . 3) . 4)))
 
+; another combination of dot-pairs
 (print '((1 . 2) . (3 . 4)))
 
 ; this is proper list
