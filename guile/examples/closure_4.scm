7cff18135369ebcb978679c30f016aac542904e1
Pavel Tisnovsky <ptisnovs@redhat.com>
2021-02-15 09:00:29 +0100

    Updated comments in closure_4.scm

--- guile/examples/closure_4.scm
+++ guile/examples/closure_4.scm
@@ -1,7 +1,7 @@
 ; Examples for Guile 2.x
 
 ;
-;  (C) Copyright 2020  Pavel Tisnovsky
+;  (C) Copyright 2020, 2021  Pavel Tisnovsky
 ;
 ;  All rights reserved. This program and the accompanying materials
 ;  are made available under the terms of the Eclipse Public License v1.0
@@ -12,19 +12,24 @@
 ;      Pavel Tisnovsky
 ;
 
+; helper function to print a value with newline appended
 (define (print item)
   (display item)
   (newline))
 
+; closure construction
 (define (get-counter)
   (let ((i -1))
+    ; function with value closed over
     (lambda ()
       (set! i (+ i 1))
       i)))
 
+; construct two closures
 (define counter1 (get-counter))
 (define counter2 (get-counter))
 
+; usage of closures
 (print (counter1))
 (print (counter1))
 (print (counter1))
@@ -36,4 +41,3 @@
 (print (counter1))
 (print (counter1))
 (print (counter1))
-
