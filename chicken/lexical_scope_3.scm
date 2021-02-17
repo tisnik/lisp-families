a13af78a73f73d5b24510d616c60d1873d6da1fe
Pavel Tisnovsky <ptisnovs@redhat.com>
2021-02-17 08:34:20 +0100

    Comments and indentation in lexical_scope_3.scm

--- chicken/lexical_scope_3.scm
+++ chicken/lexical_scope_3.scm
@@ -1,7 +1,7 @@
 ; Examples prepared for Chicken Scheme
 
 ;
-;  (C) Copyright 2020  Pavel Tisnovsky
+;  (C) Copyright 2020, 2021  Pavel Tisnovsky
 ;
 ;  All rights reserved. This program and the accompanying materials
 ;  are made available under the terms of the Eclipse Public License v1.0
@@ -13,16 +13,16 @@
 ;
 
 (define (print item)
-     (display item)
-     (newline))
+  (display item)
+  (newline))
 
 (define x 1)
 (define y 2)
 
 (define (add x y)
-    ; rozsah (scope) je lokální!
-    (set! x (+ x y))
-    x)
+  ; rozsah (scope) je lokální!
+  (set! x (+ x y))
+  x)
 
 (print (add x y))
 (print (add x y))
