4de98ba2c279038483f4219cb794951ad95e49d2
Pavel Tisnovsky <ptisnovs@redhat.com>
2021-02-17 08:34:20 +0100

    Comments and indentation in pi2.scm

--- chicken/pi2.scm
+++ chicken/pi2.scm
@@ -1,7 +1,7 @@
 ; Examples prepared for Chicken Scheme
 
 ;
-;  (C) Copyright 2020  Pavel Tisnovsky
+;  (C) Copyright 2020, 2021  Pavel Tisnovsky
 ;
 ;  All rights reserved. This program and the accompanying materials
 ;  are made available under the terms of the Eclipse Public License v1.0
@@ -12,6 +12,7 @@
 ;      Pavel Tisnovsky
 ;
 
+; Pi computation
 (: compute-pi (fixnum -> float))
 (define (compute-pi n)
   (let ((pi (the float 4.0)))
@@ -20,6 +21,7 @@
       (set! pi (* pi (/ (- i 1) i) (/ (+ i 1) i))))
     pi))
 
+; display table with Pi values
 (do ((n 1 (* n 2)))
   ((> n 10000000))
   (display n)
