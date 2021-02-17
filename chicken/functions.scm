e542c0a3d9b01606fbc8da79cd06fad59657e73c
Pavel Tisnovsky <ptisnovs@redhat.com>
2021-02-17 08:34:20 +0100

    Comments and indentation in functions.scm

--- chicken/functions.scm
+++ chicken/functions.scm
@@ -1,7 +1,7 @@
 ; Examples prepared for Chicken Scheme
 
 ;
-;  (C) Copyright 2020  Pavel Tisnovsky
+;  (C) Copyright 2020, 2021  Pavel Tisnovsky
 ;
 ;  All rights reserved. This program and the accompanying materials
 ;  are made available under the terms of the Eclipse Public License v1.0
@@ -12,9 +12,10 @@
 ;      Pavel Tisnovsky
 ;
 
+; helper function to print a value with newline appended
 (define (print item)
-     (display item)
-     (newline))
+  (display item)
+  (newline))
 
 ; anonymous function
 (lambda (x y) (+ x y))
