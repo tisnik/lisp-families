3f51ff537f67ca464f2379c0b1236534bd485674
Pavel Tisnovsky <ptisnovs@redhat.com>
2021-02-17 08:34:20 +0100

    Comments and indentation in lexical_scope_4.scm

--- chicken/lexical_scope_4.scm
+++ chicken/lexical_scope_4.scm
@@ -1,7 +1,7 @@
 ; Examples prepared for Chicken Scheme
 
 ;
-;  (C) Copyright 2020  Pavel Tisnovsky
+;  (C) Copyright 2020, 2021  Pavel Tisnovsky
 ;
 ;  All rights reserved. This program and the accompanying materials
 ;  are made available under the terms of the Eclipse Public License v1.0
@@ -13,32 +13,32 @@
 ;
 
 (define (print item)
-     (display item)
-     (newline))
+  (display item)
+  (newline))
 
 (define x 1)
 (define y 2)
 
 (define (add x y)
-    (+ x y))
+  (+ x y))
 
 (print (add x y))
 
 (print
-    (let ((x 10) 
-          (y 20)) 
-          (add x y)))
+  (let ((x 10) 
+        (y 20)) 
+    (add x y)))
 
 
 (set! x 10)
 (print (add x y))
 
 (print
-    (let ((x 10) 
-          (y 20)) 
-          (add x y)))
+  (let ((x 10) 
+        (y 20)) 
+    (add x y)))
 
 (print
-    (let ((x 100))
-          (add x y)))
+  (let ((x 100))
+    (add x y)))
 
