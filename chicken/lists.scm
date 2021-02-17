be822866ee26d8d66ae8f47199bc35c304fa7726
Pavel Tisnovsky <ptisnovs@redhat.com>
2021-02-17 08:34:20 +0100

    Comments and indentation in lists.scm

--- chicken/lists.scm
+++ chicken/lists.scm
@@ -1,7 +1,7 @@
 ; Examples prepared for Chicken Scheme
 
 ;
-;  (C) Copyright 2020  Pavel Tisnovsky
+;  (C) Copyright 2020, 2021  Pavel Tisnovsky
 ;
 ;  All rights reserved. This program and the accompanying materials
 ;  are made available under the terms of the Eclipse Public License v1.0
@@ -13,8 +13,8 @@
 ;
 
 (define (print item)
-     (display item)
-     (newline))
+  (display item)
+  (newline))
 
 (print '(1 2 3 4))
 
