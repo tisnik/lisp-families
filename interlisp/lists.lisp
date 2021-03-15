78f98186ca97fa1e2bd4b5ae2446d70cc1edf0d9
Pavel Tisnovsky <ptisnovs@redhat.com>
2021-03-15 08:40:06 +0100

    Working with lists in Interlisp

---
+++ interlisp/lists.lisp
@@ -0,0 +1,22 @@
+(print "Empty list")
+(print '())
+
+(print "A list")
+(print '(1 2 3 4))
+
+(print "Another list")
+(print (list 1 2 3 4))
+
+(setq a '(1 2 3 4))
+
+(print "car")
+(print (car a))
+
+(print "cdr")
+(print (cdr a))
+
+(print "cadr")
+(print (cadr a))
+
+(print "cddr")
+(print (cddr a))
