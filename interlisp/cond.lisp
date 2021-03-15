bb923446e03ac16135cb808434737b9cf0f2194d
Pavel Tisnovsky <ptisnovs@redhat.com>
2021-03-15 08:40:05 +0100

    Conditions in Interlisp

---
+++ interlisp/cond.lisp
@@ -0,0 +1,21 @@
+(setq n 10)
+
+(cond
+      ((< n 0)      'negative)
+      ((> n 0)      'positive)
+      ((zerop n)    'zero))
+
+(setq n -10)
+
+(cond
+      ((< n 0)      'negative)
+      ((> n 0)      'positive)
+      ((zerop n)    'zero))
+
+(setq n 0)
+
+(cond
+      ((< n 0)      'negative)
+      ((> n 0)      'positive)
+      ((zerop n)    'zero))
+
