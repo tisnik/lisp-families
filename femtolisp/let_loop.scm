4d13f46f6af617e854969553da03992e24883417
Pavel Tisnovsky <ptisnovs@redhat.com>
2022-01-17 09:00:41 +0100

    Loop based on named let

---
+++ femtolisp/let_loop.scm
@@ -0,0 +1,27 @@
+; Examples prepared for Femtolisp
+
+;
+;  (C) Copyright 2022  Pavel Tisnovsky
+;
+;  All rights reserved. This program and the accompanying materials
+;  are made available under the terms of the Eclipse Public License v1.0
+;  which accompanies this distribution, and is available at
+;  http://www.eclipse.org/legal/epl-v10.html
+;
+;  Contributors:
+;      Pavel Tisnovsky
+;
+
+; helper function to print a value with newline appended
+(define (print item)
+     (princ item)
+     (newline)
+     "")
+
+(define (let-loop)
+  (let loop ((i 0))
+    (cond ((> i 10))
+          (else (print i)
+                (loop (+ i 1))))))
+
+(let-loop)
