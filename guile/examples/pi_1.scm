ea305e4722e09579414261e8eb654556aafa3687
Pavel Tisnovsky <ptisnovs@redhat.com>
2022-01-18 08:30:46 +0100

    Pi computation

---
+++ guile/examples/pi_1.scm
@@ -0,0 +1,27 @@
+; Examples for Guile 2.x
+
+;
+;  (C) Copyright 2022  Pavel Tisnovsky
+;
+;  All rights reserved. This program and the accompanying materials
+;  are made available under the terms of the Eclipse Public License v1.0
+;  which accompanies this distribution, and is available at
+;  http://www.eclipse.org/legal/epl-v10.html
+;
+;  Contributors:
+;      Pavel Tisnovsky
+;
+
+(define (compute-pi n)
+  (let ((pi 4.0))
+    (do ((i 3 (+ i 2)))
+      ((> i (+ n 2)))
+      (set! pi (* pi (/ (- i 1) i) (/ (+ i 1) i))))
+    pi))
+ 
+(do ((n 1 (* n 2)))
+  ((> n 10000000))
+  (display n)
+  (display " ")
+  (display (compute-pi n))
+  (newline))
