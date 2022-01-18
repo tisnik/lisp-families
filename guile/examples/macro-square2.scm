02df16a7a195d1829ad7946ab7016eff0997ec33
Pavel Tisnovsky <ptisnovs@redhat.com>
2022-01-18 08:30:46 +0100

    Macros

---
+++ guile/examples/macro-square2.scm
@@ -0,0 +1,26 @@
+; Examples for Guile 2.x
+
+;
+;  (C) Copyright 2022  Pavel Tisnovsky
+;
+;  All rights reserved. This program and the accompanying materials
+;  are made available under the terms of the Eclipse Public License v1.0
+;  which accompanies this distribution, and is available at
+;  http://www.eclipse.org/legal/epl-v10.html
+;
+;  Contributors:
+;      Pavel Tisnovsky
+;
+
+(define-syntax-rule (square x)
+    (let ([tmp x])
+         (* tmp tmp)))
+
+(display (square 10))
+(newline)
+
+(display (macroexpand '(square 10)))
+(newline)
+
+(display (macroexpand '(square (+ 1 2))))
+(newline)
