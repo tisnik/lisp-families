d77aea994082ae04cf176ab036eed3e162610ff3
Pavel Tisnovsky <ptisnovs@redhat.com>
2022-01-18 08:30:46 +0100

    Preparing for parallel computing Pi

---
+++ guile/examples/pi_for_parallel.scm
@@ -0,0 +1,33 @@
+; Examples for Guile 2.x
+
+;
+;  (C) Copyright 2022  Pavel Tisnovsky
+;
+;  All rights reserved. This program and the accompanying materials
+;  are made available under the terms of the Eclipse Public License v1.0
+;  which accompanies this distribution, and is available at
+;  http://www.eclipse.org/legal/epl-v10.html
+;
+;  Contributors:
+;      Pavel Tisnovsky
+;
+
+(define (compute-pi n)
+  (let ((pi 4.0))
+    (do ((i 3 (+ i 2)))
+      ((> i (+ n 2)))
+      (set! pi (* pi (/ (- i 1) i) (/ (+ i 1) i))))
+    pi))
+ 
+(define (pi-for n)
+  (display n)
+  (display "\t")
+  (display (compute-pi n))
+  (newline))
+
+(define (power-of-two n maximum)
+  (if (< n maximum)
+      (cons n (power-of-two (* n 2) maximum))
+      (list n)))
+
+(map pi-for (power-of-two 1 5000000))
