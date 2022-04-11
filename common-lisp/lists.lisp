7e7831d28f5abd70334e1ba436091c4176f3df24
Pavel Tisnovsky <ptisnovs@redhat.com>
2022-04-11 08:45:06 +0200

    List manipulations

---
+++ common-lisp/lists.lisp
@@ -0,0 +1,24 @@
+(print "Empty list")
+(print '())
+
+(print "A list")
+(print '(1 2 3 4))
+
+(print "Another list")
+(print (list 1 2 3 4))
+
+(defvar a '(1 2 3 4))
+
+(print "car")
+(print (car a))
+
+(print "cdr")
+(print (cdr a))
+
+(print "cadr")
+(print (cadr a))
+
+(print "cddr")
+(print (cddr a))
+
+(quit)
