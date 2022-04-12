a5cdc8d8ea2d9e4f1255da3c7177fdacf1729456
Pavel Tisnovsky <ptisnovs@redhat.com>
2022-04-12 07:55:27 +0200

    Loop macro in Common Lisp

---
+++ common-lisp/loop_for_in.lisp
@@ -0,0 +1,18 @@
+(defvar lst '(1 2 3 4 5 6 7 8 9 10))
+
+(print
+    (loop for i in lst
+          collect i))
+
+(print
+    (loop for i in lst
+          collect (* i i)))
+
+
+(defun factorial (n)
+    (cond ((zerop n) 1)
+          (T (* n (factorial (- n 1))))))
+
+(print
+    (loop for i in lst
+          collect (factorial i)))
