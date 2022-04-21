a6921e669e8edd2b16896cdb7143ca35c106266a
Pavel Tisnovsky <ptisnovs@redhat.com>
2022-04-21 10:00:14 +0200

    Fixed issue #549

--- tinyscheme/examples/do_loop.scm
+++ tinyscheme/examples/do_loop.scm
@@ -1,5 +1,5 @@
 ;
-;  (C) Copyright 2020  Pavel Tisnovsky
+;  (C) Copyright 2020, 2021, 2022  Pavel Tisnovsky
 ;
 ;  All rights reserved. This program and the accompanying materials
 ;  are made available under the terms of the Eclipse Public License v1.0
@@ -10,7 +10,10 @@
 ;      Pavel Tisnovsky
 ;
 
+; pocitadlo
 (define i 0)
+
+; tabulka sekvence hodnot
 (do ()
   ((>= i 10))
   (display i)
